0cc6daa075028921921e6af216ba9bec4884e5e7
dtroode <davidkis113@gmail.com>
2019-09-23 18:02:36 +0300

    Add new header link

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,10 +2,8 @@ window.addEventListener("scroll", () => {
   let y = Math.abs(intr.getBoundingClientRect().y) + 32;
   if (y >= intr.getBoundingClientRect().height) {
     head.classList.add("header--scrolled");
-    topLink.classList.add("nav--enabled");
   }
   else {
     head.classList.remove("header--scrolled");
-    topLink.classList.remove("nav--enabled");
   }
 })
