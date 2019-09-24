c87a88465fa297a5323d1fd44f9ff154b5dc9c2b
dtroode <davidkis113@gmail.com>
2019-09-24 15:35:19 +0300

    Add mobile nav

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,3 +1,5 @@
+const mobNav = document.querySelector("#mobNav");
+
 window.addEventListener("scroll", () => {
   let y = Math.abs(intr.getBoundingClientRect().y) + 32;
   if (y >= intr.getBoundingClientRect().height) {
@@ -6,4 +8,32 @@ window.addEventListener("scroll", () => {
   else {
     head.classList.remove("header--scrolled");
   }
+})
+
+function closeNav() {
+  mobNav.style.opacity = "0";
+  setTimeout(() => {
+    mobNav.classList.remove("mob-nav--opened");
+
+  }, 100);
+  document.body.style.overflowY = "scroll";
+}
+
+document.querySelector("#open").addEventListener("click", () => {
+  mobNav.style.opacity = "1";
+  setTimeout(() => {
+    mobNav.classList.add("mob-nav--opened");
+
+  }, 100);
+  document.body.style.overflowY = "hidden";
+})
+
+document.querySelector("#close").addEventListener("click", () => {
+  closeNav()
+})
+
+mobNav.querySelectorAll("a").forEach(a => {
+  a.addEventListener("click", () => {
+    closeNav()
+  })
 })
