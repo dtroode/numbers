f72fc2f2f841780e23eca4e6540d123c3721d9d2
dtroode <davidkis113@gmail.com>
2020-01-03 20:32:52 +0300

    Add an offline support

---
+++ src/service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'static-cache';
+const DATA_CACHE_NAME = 'data-cache';
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html'
+]
+
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  if (evt.request.url.includes('/')) {
+    console.log('[Service Worker] Fetch (data)', evt.request.url);
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+          .then((response) => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+      }));
+    return;
+  }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request)
+        .then((response) => {
+          return response || fetch(evt.request);
+        });
+    })
+  );
+
+});
